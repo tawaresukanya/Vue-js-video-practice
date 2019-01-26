23033ed9480b65b47738df5615852aace5e04f7c
tawaresukanya <46738003+tawaresukanya@users.noreply.github.com>
2019-01-26 13:47:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+Vue.component('task', {
+
+    template: '<li><slot></slot></li>',
+
+
+    // data() {
+    //     return{
+    //         message: 'Footer'
+    //     };
+    // }
+
+});
+
+
+new  Vue({
+
+    el: '#root'
+
+});
